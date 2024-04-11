da9e0b479ca66f25aaedb5ef3c1762a805faeccc
Reniel George Mababa <mabsrencode@gmail.com>
2024-04-12 06:40:10 +0800

    form modal

--- frontend/src/Components/Forms/SignInForm.jsx
+++ frontend/src/Components/Forms/SignInForm.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
-const SignInForm = () => {
+import React, { useState } from 'react'
+import axios from 'axios'
+const SignInForm = ({ open }) => {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const handleSignIn = async () => {
+        const formData = { username: username, password: password }
+        await axios.post("/api/auth", formData)
+    }
     return (
-        <div>SignInForm</div>
+        <div className={`${open ? "block" : "hidden"} z-30 fixed flex flex-col bg-white shadow-xl rounded-xl py-3 px-6`}>
+            <input onChange={(e) => setUsername(e.target.value)} type="text" name="username" id="username" />
+            <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" />
+            <button onClick={handleSignIn}>Sign In</button>
+        </div>
     )
 }
 
--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from "../../Assets/logo/logo.svg";
+import SignInForm from '../Forms/SignInForm';
 const Header = () => {
     const [navClick, setNavClick] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
     const handleClick = () => {
         setNavClick(!navClick)
         if (!navClick) {
@@ -16,7 +18,13 @@ const Header = () => {
             document.getElementById("line2").style.rotate = "0deg"
         }
     };
-
+    const handleOpenSignInForm = () => {
+        setOpenModal(!openModal);
+        setNavClick(!navClick);
+        document.getElementById("line").style.rotate = "0deg"
+        document.getElementById("line2").style.marginTop = "8px"
+        document.getElementById("line2").style.rotate = "0deg"
+    }
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 100) {
@@ -32,58 +40,61 @@ const Header = () => {
         };
     }, []);
     return (
-        <nav className={`fixed z-20 w-full ${navClick && "bg-white"} bg-transparent ${isScrolled && "backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 shadow-md"}`}>
-            <div className="container m-auto px-2 md:px-12 lg:px-7">
-                <div className={`flex flex-wrap justify-between items-center py-3 ${navClick && "gap-6"} lg:gap-6 md:py-4 md:gap-0`}>
-                    <div className="w-full px-6 flex justify-between items-center lg:w-max md:px-0">
-                        <Link to={"/"} aria-label="logo" className="flex space-x-2 items-center">
-                            <img src={Logo} className='w-12' alt="logo" />
-                            <span className="text-2xl font-bold text-yellow-900">Food <span className="text-yellow-700">Gallery</span></span>
-                        </Link>
+        <>
+            <nav className={`fixed z-20 w-full ${navClick && "bg-white"} bg-transparent ${isScrolled && "backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 shadow-md"}`}>
+                <div className="container m-auto px-2 md:px-12 lg:px-7">
+                    <div className={`flex flex-wrap justify-between items-center py-3 ${navClick && "gap-6"} lg:gap-6 md:py-4 md:gap-0`}>
+                        <div className="w-full px-6 flex justify-between items-center lg:w-max md:px-0">
+                            <Link to={"/"} aria-label="logo" className="flex space-x-2 items-center">
+                                <img src={Logo} className='w-12' alt="logo" />
+                                <span className="text-2xl font-bold text-yellow-900">Food <span className="text-yellow-700">Gallery</span></span>
+                            </Link>
 
-                        <button onClick={handleClick} aria-label="humburger" id="hamburger" className="relative w-10 h-10 -mr-2 lg:hidden">
-                            <div aria-hidden="true" id="line" className="inset-0 w-6 h-0.5 m-auto rounded bg-yellow-900 transtion duration-300"></div>
-                            <div aria-hidden="true" id="line2" className="inset-0 w-6 h-0.5 mt-2 m-auto rounded bg-yellow-900 transtion duration-300"></div>
-                        </button>
-                    </div>
-
-                    <div className={`${navClick ? "scale-100 h-full p-6 md:mt-12" : "scale-0 h-0"} origin-top transition-all w-full lg:scale-100  lg:flex flex-wrap justify-end items-center space-y-6  rounded-xl  bg-yellow-100 lg:space-y-0 lg:p-0 lg:flex-nowrap lg:bg-transparent lg:w-7/12`}>
-                        <div className="text-gray-600 lg:pr-4">
-                            <ul className="space-y-6 tracking-wide font-medium text-sm md:flex md:space-y-0">
-                                <li>
-                                    <Link to={"/categories"} className="block md:px-4 transition hover:text-yellow-700">
-                                        All Categories
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={"/country"} className="block md:px-4 transition hover:text-yellow-700">
-                                        Country
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to={""} className="block md:px-4 transition hover:text-yellow-700">
-                                        About
-                                    </Link>
-                                </li>
-                            </ul>
+                            <button onClick={handleClick} aria-label="humburger" id="hamburger" className="relative w-10 h-10 -mr-2 lg:hidden">
+                                <div aria-hidden="true" id="line" className="inset-0 w-6 h-0.5 m-auto rounded bg-yellow-900 transtion duration-300"></div>
+                                <div aria-hidden="true" id="line2" className="inset-0 w-6 h-0.5 mt-2 m-auto rounded bg-yellow-900 transtion duration-300"></div>
+                            </button>
                         </div>
 
-                        <div className="w-full space-y-2 border-yellow-200 lg:space-y-0 md:w-max lg:border-l">
-                            <button type="button" title="Sign Up" className="w-full py-3 px-6 text-center rounded-full transition active:bg-yellow-200 focus:bg-yellow-100 sm:w-max">
-                                <p className="block text-yellow-800 font-semibold text-sm">
-                                    Sign up
-                                </p>
-                            </button>
-                            <button type="button" title="Login" className="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max">
-                                <p className="block text-yellow-900 font-semibold text-sm">
-                                    Login
-                                </p>
-                            </button>
+                        <div className={`${navClick ? "scale-100 h-full p-6 md:mt-12" : "scale-0 h-0"} origin-top transition-all w-full lg:scale-100  lg:flex flex-wrap justify-end items-center space-y-6  rounded-xl  bg-yellow-100 lg:space-y-0 lg:p-0 lg:flex-nowrap lg:bg-transparent lg:w-7/12`}>
+                            <div className="text-gray-600 lg:pr-4">
+                                <ul className="space-y-6 tracking-wide font-medium text-sm md:flex md:space-y-0">
+                                    <li>
+                                        <Link to={"/categories"} className="block md:px-4 transition hover:text-yellow-700">
+                                            All Categories
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={"/country"} className="block md:px-4 transition hover:text-yellow-700">
+                                            Country
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to={""} className="block md:px-4 transition hover:text-yellow-700">
+                                            About
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="w-full space-y-2 border-yellow-200 lg:space-y-0 md:w-max lg:border-l">
+                                <button type="button" title="Sign Up" className="w-full py-3 px-6 text-center rounded-full transition active:bg-yellow-200 focus:bg-yellow-100 sm:w-max">
+                                    <p className="block text-yellow-800 font-semibold text-sm">
+                                        Sign up
+                                    </p>
+                                </button>
+                                <button onClick={handleOpenSignInForm} type="button" title="Login" className="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max">
+                                    <p className="block text-yellow-900 font-semibold text-sm">
+                                        Login
+                                    </p>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <SignInForm open={openModal} />
+        </>
     )
 }
 
