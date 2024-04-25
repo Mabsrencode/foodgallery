59ca664935bb75afa9d316dc8a200e8e002ffc33
Reniel George Mababa <mabsrencode@gmail.com>
2024-04-25 23:01:32 +0800

    test

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    // origin: "*",
+    // origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -26,11 +26,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(express.static(path.join(__dirname, "build")));
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 
-//   // ../frontend/
-// });
+  // ../frontend/
+});
 
 // const dbUrl = process.env.DB_URL;
 const PORT = process.env.PORT || 4000;
