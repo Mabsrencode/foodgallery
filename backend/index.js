c2f6b20483370db13a4e27f2267ef8d6bac973dd
Reniel George Mababa <mabsrencode@gmail.com>
2024-07-16 13:28:29 +0800

    cors

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://foodgallery.onrender.com",
-    // origin: "*",
+    // origin: "https://foodgallery.onrender.com",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
